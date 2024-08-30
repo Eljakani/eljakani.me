819c2268f11abdfa0c3a83dcdcf6df0f5050145c
MrPatcher <eljakaniyassin@gmail.com>
2024-08-30 14:08:55 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  // PORT 
+  server: {
+    port: 3000
+  },
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), icon()]
+});
