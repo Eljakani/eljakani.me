a9cdc6b3a21ce91cd32a40510ffdcf5bf6c7f854
MrPatcher <eljakaniyassin@gmail.com>
2024-09-08 19:47:21 +0100

    finalized layout, one project added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,27 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  // PORT 
+  // PORT
   server: {
     port: 3000
   },
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), icon()]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    icon(),
+    mdx()
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  }
 });
