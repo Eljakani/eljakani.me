d3fa9084a6c7af250e424f606fe21c6d77781671
MrPatcher <eljakaniyassin@gmail.com>
2024-09-09 02:04:46 +0100

    fixed some build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,25 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   // PORT
   server: {
     port: 3000
   },
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    icon(),
-    mdx()
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), icon(), mdx()],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       wrap: true
     }
-  }
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
