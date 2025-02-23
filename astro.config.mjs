a35fb9b4c8c877bc31821eed5388fc95c34e5900
MrPatcher <eljakaniyassin@gmail.com>
2025-02-23 02:46:01 +0100

    Configure for Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,35 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import cloudflare from '@astrojs/cloudflare';
 
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
-  // PORT
-  server: {
-    port: 3000
-  },
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), icon(), mdx()],
+  integrations: [
+    react(), 
+    tailwind({
+      applyBaseStyles: false
+    }), 
+    icon(), 
+    mdx()
+  ],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       wrap: true
     }
   },
-  // Build configuration
   build: {
-    // Exclude node_modules and .dist folders from processing
     exclude: ['**/node_modules/**', '**/.dist/**']
   },
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  image: {
+    service: {
+      entrypoint: '@astrojs/cloudflare/assets'
+    }
+  }
 });
